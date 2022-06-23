c7390139ea8e4550adf42e50b8fd9f06a5d37b3e
patriciapallares <patriciapallaresgonzalez@gmail.com>
2022-06-23 10:40:25 +0200

    fixed burger to be fixed and close when clicked

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -16,27 +16,57 @@ function Navbar(props) {
       <nav>
         <ul className='navbar__list'>
           <li className=' '>
-            <Link className='navbar__list__link ' to='/'>Inicio </Link>
+            <Link
+              className='navbar__list__link '
+              to='/'
+              onClick={handleXmarkClick}
+            >
+              Inicio 
+            </Link>
           </li>
           <span className='navbar__dash   '> - </span>
 
           <li className=' hidden'>
-            <Link className='navbar__list__link ' to='/CV'>CV </Link>
+            <Link
+              className='navbar__list__link '
+              to='/CV'
+              onClick={handleXmarkClick}
+            >
+              CV 
+            </Link>
           </li>
           <span className='navbar__dash hidden  '> - </span>
 
           <li className=' '>
-            <Link className='navbar__list__link ' to='/portfolio'>Portfolio </Link>
+            <Link
+              className='navbar__list__link '
+              to='/portfolio'
+              onClick={handleXmarkClick}
+            >
+              Portfolio 
+            </Link>
           </li>
           <span className='navbar__dash  hidden '> - </span>
 
           <li className=' hidden'>
-            <Link className='navbar__list__link ' to='/contacto'>Contacto </Link>
+            <Link
+              className='navbar__list__link '
+              to='/contacto'
+              onClick={handleXmarkClick}
+            >
+              Contacto 
+            </Link>
           </li>
           <span className='navbar__dash  hidden  '> - </span>
 
           <li className=' hidden '>
-            <Link className='navbar__list__link ' to='/blog'>Blog </Link>
+            <Link
+              className='navbar__list__link '
+              to='/blog'
+              onClick={handleXmarkClick}
+            >
+              Blog 
+            </Link>
           </li>
           <span className='navbar__dash  hidden '> - </span>
         </ul>
