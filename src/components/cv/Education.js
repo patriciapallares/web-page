7dec894ee6c8ae50441983ed6f4f6d67ee816eb6
patriciapallares <patriciapallaresgonzalez@gmail.com>
2022-08-08 18:39:47 +0200

    change classes names

--- src/components/cv/Education.js
+++ src/components/cv/Education.js
@@ -1,12 +1,12 @@
 function Education(props) {
   return (
     <div>
-      <div className='education__date'>{props.date}</div>
+      <div className='cv__component__date'>{props.date}</div>
 
-      <div className='education__info'>
-        <h3 className='education__role'>{props.title}</h3>
-        <p className='education__location'>{props.location}</p>
-        <p className='education__description'>{props.description}</p>
+      <div className='cv__component__info'>
+        <h3 className='cv__component__role'>{props.title}</h3>
+        <p className='cv__component__location'>{props.location}</p>
+        <p className='cv__component__description'>{props.description}</p>
       </div>
     </div>
   );
--- src/components/cv/Experience.js
+++ src/components/cv/Experience.js
@@ -1,12 +1,12 @@
 function Experience(props) {
   return (
     <div>
-      <div className='experience__date'>{props.date}</div>
+      <div className='cv__component__date'>{props.date}</div>
 
-      <div className='experience__info'>
-        <h3 className='experience__role'>{props.role}</h3>
-        <p className='experience__location'>{props.location}</p>
-        <p className='experience__description'>{props.description}</p>
+      <div className='cv__component__info'>
+        <h3 className='cv__component__role'>{props.role}</h3>
+        <p className='cv__component__location'>{props.location}</p>
+        <p className='cv__component__description'>{props.description}</p>
       </div>
     </div>
   );
